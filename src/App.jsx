2dac4659292ee1c89fcaf7460aabbf4a51868fd2
wallapsu-data <wallapsu.data@gmail.com>
2026-06-14 16:28:34 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from './context/ThemeContext.jsx'
 import LandingPage from './pages/LandingPage.jsx'
 import AppLayout from './components/layout/AppLayout.jsx'
 import UploadPage from './pages/UploadPage.jsx'
@@ -8,17 +9,16 @@ import DashboardPage from './pages/DashboardPage.jsx'
 
 export default function App() {
   return (
-    <Routes>
-      {/* Public landing page */}
-      <Route path="/" element={<LandingPage />} />
-
-      {/* App routes with sidebar layout */}
-      <Route path="/app" element={<AppLayout />}>
-        <Route index element={<DashboardPage />} />
-        <Route path="upload" element={<UploadPage />} />
-        <Route path="results/:documentId" element={<ResultsPage />} />
-        <Route path="history" element={<HistoryPage />} />
-      </Route>
-    </Routes>
+    <ThemeProvider>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route index element={<DashboardPage />} />
+          <Route path="upload" element={<UploadPage />} />
+          <Route path="results/:documentId" element={<ResultsPage />} />
+          <Route path="history" element={<HistoryPage />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   )
 }
